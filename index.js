d6697d55265fdcfa6eed1fee2f872d9f74be76d5
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2021-05-04 00:38:20 -0500

    added cors support

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const routesRouter = require('./routes/admin_routes');
 const stopsRouter = require('./routes/admin_stops');
 const searchRouter = require('./routes/admin_search');
-
 const app = express();
 const port = 3000
 
-app.use(express.json()) // for parsing application/json
+app.use(cors());
+app.use(express.json()); // for parsing application/json
 
 app.use('/routes', routesRouter);
 app.use('/stops', stopsRouter);
